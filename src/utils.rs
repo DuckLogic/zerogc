927e525bda9eb11b15eae18d88051437af8199ae
Techcable <git@techcable.net>
2024-04-29 10:24:25 -0700

    utils: Remove alignment check from transmute_arbitrary

--- src/utils.rs
+++ src/utils.rs
@@ -2,38 +2,66 @@ pub(crate) mod layout_helpers;
 
 pub(crate) use self::layout_helpers::LayoutExt;
 use crate::Collect;
+use crate::Collect;
 use std::mem::ManuallyDrop;
+use std::mem::{self, ManuallyDrop};
 
+/// Transmute one type into another,
+/// without doing compile-time checks for sizes.
+///
+/// The difference between [`mem::transmute`] is that this function
+/// does not attempt to check sizes at compile time.
+/// In the regular [`mem::transmute`] function, an compile-time error occurs
+/// if the sizes can't be proved equal at compile time.
+/// This function does `assert_eq!` at runtime instead.
+///
+/// In some contexts, the sizes of types are statically unknown,
+/// so a runtime assertion is better than a static compile-time check.
+///
+/// ## Safety
+/// See [`mem::transmute`] for full details on safety.
+///
+/// The sizes of the two types must exactly match,
+/// but unlike [`mem::transmute`] this is not checked at compile time.
+///
+/// Because transmute is a by-value operation,
+/// the alignment of the transmuted values themselves is not a concern.
 #[inline(always)]
-pub unsafe fn transmute_arbitrary<T, U>(val: T) -> U {
-    if std::mem::size_of::<T>() != std::mem::size_of::<U>()
-        || std::mem::align_of::<T>() != std::mem::align_of::<U>()
-    {
-        mismatch_transmute_size::<T, U>()
+#[track_caller] // for the case where sizes don't match
+pub unsafe fn transmute_arbitrary<Src, Dst>(val: Src) -> Dst {
+    const SIZE_MATCHES: bool = std::mem::size_of::<Src>() == std::mem::size_of::<Dst>();
+    if SIZE_MATCHES {
+        let src: ManuallyDrop<Src> = ManuallyDrop::new(val);
+        std::mem::transmute_copy(&src as &Src)
+    } else {
+        mismatch_transmute_sizes(
+            TransmuteTypeInfo::new::<Src>(),
+            TransmuteTypeInfo::new::<Dst>(),
+        )
     }
-    ManuallyDrop::into_inner(std::ptr::read::<ManuallyDrop<U>>(&ManuallyDrop::new(val)
-        as *const ManuallyDrop<T>
-        as *const ManuallyDrop<U>))
+}
+
+struct TransmuteTypeInfo {
+    size: usize,
+    type_name: &'static str,
+}
+impl TransmuteTypeInfo {
+    #[inline]
+    pub fn new<T>() -> Self {
+        TransmuteTypeInfo {
+            size: std::mem::size_of::<T>(),
+            type_name: std::any::type_name::<T>(),
+        }
+    }
 }
 
 #[cold]
-fn mismatch_transmute_size<T, U>() -> ! {
-    let original_type_name = std::any::type_name::<T>();
-    let collected_type_name = std::any::type_name::<U>();
-    let sizes = [
-        ("sizes", std::mem::size_of::<T>(), std::mem::size_of::<T>()),
-        (
-            "alignments",
-            std::mem::align_of::<T>(),
-            std::mem::align_of::<U>(),
-        ),
-    ];
-    for (value_name, original_size, collected_size) in sizes {
-        assert_eq!(
-            original_size, collected_size,
-            "Mismatched {} between T `{}` and U `{}`",
-            value_name, original_type_name, collected_type_name
-        );
-    }
-    unreachable!("sizes & alignments unexpectedly matched")
+#[track_caller]
+fn mismatch_transmute_sizes(src: TransmuteTypeInfo, dst: TransmuteTypeInfo) -> ! {
+    assert_eq!(
+        src.size, dst.size,
+        "Mismatched size between Src `{}` and Dst `{}`",
+        src.type_name, dst.type_name
+    );
+    unreachable!() // sizes actually match
 }
