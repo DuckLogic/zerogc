145fab5e9fe5e2c01201942622e317d6e9ae0a75
Techcable <git@techcable.net>
2024-05-08 14:32:40 -0700

    Remove safepoint macros
    Will not be used with initial version of v0.3 API

--- src/prelude.rs
+++ src/prelude.rs
@@ -5,8 +5,6 @@
 //! This should really contain everything a garbage
 //! collected program needs to use the API.
 
-// macros
-pub use crate::{freeze_context, safepoint, safepoint_recurse, unfreeze_context};
 // Basic collector types
 pub use crate::{Gc, GcContext, GcHandle, GcSimpleAlloc, GcSystem, GcVisitor, HandleCollectorId};
 // Traits for user code to implement
