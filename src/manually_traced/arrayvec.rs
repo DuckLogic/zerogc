0bda65c17de586727b740c2ba9e4db131839f8b0
Techcable <git@techcable.net>
2024-05-25 12:43:16 -0700

    Fix usage of indexmap mutable keys in Trace
    Only offered with special extension trait `MutableKeys`.
    Drop unused `deserialize` option for macro (GcDeserialize removed).
    
    Drop unused dependencies.

--- src/manually_traced/arrayvec.rs
+++ src/manually_traced/arrayvec.rs
@@ -12,7 +12,6 @@ unsafe_gc_impl!(
     NEEDS_DROP => false,
     branded_type => ArrayString<SIZE>,
     trace_template => |self, visitor| { Ok(()) },
-    deserialize => delegate
 );
 
 unsafe_gc_impl!(
@@ -31,46 +30,4 @@ unsafe_gc_impl!(
         }
         Ok(())
     },
-    deserialize => |ctx, deserializer| {
-        use core::marker::PhantomData;
-        use crate::CollectorId;
-        use crate::serde::{GcDeserialize, GcDeserializeSeed};
-        use serde::de::{Visitor, Error, SeqAccess};
-        struct ArrayVecVisitor<
-            'gc, 'de, Id: CollectorId,
-            T: GcDeserialize<'gc, 'de, Id>,
-            const SIZE: usize
-        > {
-            ctx: &'gc Id::Context,
-            marker: PhantomData<fn(&'de ()) -> ArrayVec<T, SIZE>>
-        }
-        impl<
-            'gc, 'de, Id: CollectorId,
-            T: GcDeserialize<'gc, 'de, Id>,
-            const SIZE: usize
-        > Visitor<'de> for ArrayVecVisitor<'gc, 'de, Id, T, SIZE> {
-            type Value = ArrayVec<T, SIZE>;
-            fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-                write!(f, "a array with size <= {}", SIZE)
-            }
-            #[inline]
-            fn visit_seq<A>(self, mut access: A) -> Result<Self::Value, A::Error>
-                where A: SeqAccess<'de>, {
-                let mut values = Self::Value::new();
-                while let Some(value) = access.next_element_seed(
-                    GcDeserializeSeed::new(self.ctx)
-                )? {
-                    match values.try_push(value) {
-                        Ok(()) => {},
-                        Err(_) => {
-                            return Err(A::Error::invalid_length(SIZE + 1, &self))
-                        }
-                    }
-                }
-                Ok(values)
-            }
-        }
-        let visitor: ArrayVecVisitor<Id, T, SIZE> = ArrayVecVisitor { ctx, marker: PhantomData };
-        deserializer.deserialize_seq(visitor)
-    }
 );
